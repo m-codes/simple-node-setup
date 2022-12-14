9aaec9a57d65a22c7e3fa8898e35e72ea076f11d
m <32762403+m-codes@users.noreply.github.com>
2022-12-14 10:37:46 +0000

    :construction: bash file created
    bash file and shebang created, takes a variable a string from the user for the directory name

--- simple-node-setup.sh
+++ simple-node-setup.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+# This make the script take an argument/parameter
+# This is the directory name
+# It will also be used for the files name
+dirName="$1";
+
+# Ask the user for the folder name
+# echo "Please enter the folder name:"
+# read folderName
