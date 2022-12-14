5ae51a87a2141e170ccdb7fdca0886878f3c42c1
m <32762403+m-codes@users.noreply.github.com>
2022-12-14 10:43:40 +0000

    :sparkles: Make a directory and file from user input
    create directory and js file from user input

--- simple-node-setup.sh
+++ simple-node-setup.sh
@@ -3,3 +3,12 @@
 # This is the directory name
 # It will also be used for the files name
 dirName="$1";
+
+# Create a directory using this name
+mkdir -p "$dirName"
+
+# Change directory and go inside the diretory created
+cd "$dirName"
+
+# Create the JS file in the given directory
+touch "$dirName".js
